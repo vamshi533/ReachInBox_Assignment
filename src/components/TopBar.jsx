25e24729b320d7feb603328fbf9f33268c86dcc0
vamshi533 <107039412+vamshi533@users.noreply.github.com>
2024-08-15 20:59:33 +0530

    Update TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle";
 
 function TopBar() {
   return (
-    <div className="h-16 w-screen bg-white dark:bg-[#1F1F1F] fixed px-14 text-[#5B5F66] dark:text-white top-0 flex justify-between items-center border-b-2 dark:border-[#343A40] border-[#E0E0E0]">
+    <div className="h-14 w-screen bg-white dark:bg-[#1F1F1F] fixed px-14 text-[#5B5F66] dark:text-white top-0 flex justify-between items-center border-b-2 dark:border-[#343A40] border-[#E0E0E0]">
       <div>Onebox</div>
 
       <div className="pr-10 flex items-center">
